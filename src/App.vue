3494a4526ef3dacfdc0a836ef78944a3b5f0dff8
Kris Johnson <kristofj@d.umn.edu>
2023-10-04 09:50:10 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<!-- App.vue -->
+<script setup lang="ts">
+// typescript code goes here
+</script>
+
+<template>
+  <!-- template markup will go here -->
+</template>
+
+<style scoped>
+/** our root level styles can go here */
+</style>
